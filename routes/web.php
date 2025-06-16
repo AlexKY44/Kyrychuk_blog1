c6856ddfbf22f05f7a29eefd28b47868f557ce58
AlexKY44 <oleksii.kyrychuk@oa.edu.ua>
2025-06-16 09:42:39 +0000

    Лабораторна 11. Колекції

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\RestTestController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DiggingDeeperController;
 
 Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', PostController::class)->names('blog.posts');
@@ -21,6 +22,13 @@ Route::group($groupData, function () {
     Route::resource('categories', CategoryController::class)
         ->only($methods)
         ->names('blog.admin.categories');
+    Route::group(['prefix' => 'digging_deeper'], function () {
+
+        Route::get('collections', [DiggingDeeperController::class, 'collections'])
+
+            ->name('digging_deeper.collections');
+
+    });
     //BlogPost
     Route::resource('posts', PostController::class)
         ->except(['show'])                               //не робити маршрут для метода show
